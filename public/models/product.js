dfddfe37506cb7e9c655bf53403a3e9bfa853dc4
ong ding chao <ongdingchao@MacBook-Pro-2.attlocal.net>
2019-12-01 03:30:34 -0800

    Finished up routing of cart and admin pages, removed redundant pages (e.g add-product, since only 1 route is needed for modification purposes like Delete, edit or add. Also added Delete api routes for deletion of products in carts as well as a global deletion of the item altogether for the admin page.

--- public/models/product.js
+++ public/models/product.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const Cart = require('../models/cart');
 const p = path.join(
   path.dirname(process.mainModule.filename),
   'data',
@@ -18,7 +19,8 @@ const getProductFromFile = (cb) => {
 }
 
 module.exports = class Product {
-  constructor(title, imageURL, description,price) {
+  constructor(id,title, imageURL, description,price) {
+    this.id = id;
     this.title = title;
     this.imageURL = imageURL;
     this.description = description;
@@ -26,25 +28,45 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
+    
     getProductFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
+      if (this.id) {
+        const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          console.log(err);
+        });
+      } else{
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }  
     });
   }
 
+  static deleteById(id) {
+
+    getProductFromFile(products => {
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(prod => prod.id !== id);
+
+      fs.writeFile(p,JSON.stringify(updatedProducts), err => {
+        if(!err) {
+          Cart.deleteProduct(id,product.price);
+        }
+      });
+    });
+  }
   static fetchAll(cb) {
     getProductFromFile(cb);
   }
 
   static findById(id, cb){
     getProductFromFile(products => {
-      const product = products.find(p => {
-        console.log(p);
-        return p.id === id;
-      });
+      const product = products.find(p => p.id === id);
       cb(product);
     });
 
